c8ed2cf68b659904c940b71aa0b10e9dd0cbd758
marco.santiago <marco.perezj96@gmail.com>
2020-10-24 13:53:16 -0500

    Sugerencias/Contacto añadidas

---
+++ Trabajo_1/sug.php
@@ -0,0 +1,29 @@
+<?php 
+// Conexión a la bd
+$servername = "localhost";
+$database = "general";
+$username = "root";
+$password = "";
+//Crea la conexion
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+echo "Connected successfully";
+//declaracion de varibles
+$usuario = $_POST["user"];
+$email = $_POST["email"]; 
+$comentario = $_POST["sugerencia"];
+
+
+$sql = "INSERT INTO comments (user,email,comment) VALUES ('$usuario','$email','$comentario')";   
+
+if (mysqli_query($conn, $sql)) {
+    echo "Se ha registrado correctamente";
+} else {
+   echo "Error:" . $sql . "<br>" . mysqli_error($conn);
+}
+mysqli_close($conn);
+?>
